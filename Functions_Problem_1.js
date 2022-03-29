34011c90e797056b6a4b1293fc492bd62060213e
Dominique Morris <morrisdominique16@yahoo.com>
2022-03-28 21:26:07 -0400

    Working Problem 1! I solved it

--- Functions_Problem_1.js
+++ Functions_Problem_1.js
@@ -7,27 +7,21 @@ step 1: define a variable called longestWord
 step 2: define a Javascript function that accepts a string as a paramter
 step 3: split the string that is passed in, turning it into an array of individual words
 step 4: start looping through the array of words with a forEach loop
-step 5: check current word for length 
+step 5: compare current word for length with next word for length
+step 6: replace current word with longest word and repeat until unable
 step 6: return the value of longestWord 
 
+*/ 
 
-*/
-
-
-
-function findLongestWord() {
-  let longestWord = "The first function problem in JavaScript";
-  const myArray = longestWord.split(" ");
+function checkLongestWord(longestWord) {
+  const arrayOfWords = longestWord.split(" ");
+  let position = "";
   
-    myArray.forEach(arrayWords => {
-      if (arrayWords.length > longestWord.length){
-      longestWord = arrayWords;
-      
+  arrayOfWords.forEach(word => {
+    if(word.length > position.length){
+      position = word; 
     }
-      
-}    
-      
-); 
-        return longestWord;
+  });
+  return position;
 }
- findLongestWord(longestWord);
+console.log(checkLongestWord("I finally figured out how to code this problem."));
